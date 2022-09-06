f8b1636e0536315119d9247a4e9e56c721184cb7
Zachary Youngdale <zyoungdale@gmail.com>
2022-09-06 10:26:07 -0400

    commit tree c2585ca4dac5d64f8904d1b3c5d5a8ad88333415

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => (document.body.innerHTML = data.id))
+    .catch((error) => {
+      let message = "Unauthorized Access";
+      return (document.body.innerHTML = message);
+    });
+}
